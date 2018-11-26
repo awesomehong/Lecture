64788058d55161704a4f0c24e77ece6534e3f139
awesomehong0611@gmail.com <awesomehong0611@gmail.com>
2018-11-26 14:01:38 +0900

    Project7

--- Chapter 7/Chapter 7/IntSet.h
+++ Chapter 7/Chapter 7/IntSet.h
@@ -1,1 +1,37 @@
-#pragma once
+#ifndef ___IntSet
+#define ___IntSet
+
+typedef struct {
+	int max;
+	int num;
+	int *set;
+} IntSet;
+
+int Initialize(IntSet *s, int max);
+
+int IsMember(const IntSet *s, int n);
+
+void Add(IntSet *s, int n);
+
+void Remove(IntSet *s, int n);
+
+int Capacity(const IntSet *s);
+
+int Size(const IntSet *s);
+
+void Assign(IntSet *s1, const IntSet *s2);
+
+int Equal(const IntSet *s1, const IntSet *s2);
+
+IntSet *Union(IntSet *s1, const IntSet *s2, const IntSet *s3);
+
+IntSet *Intersection(IntSet *s1, const IntSet *s2, const IntSet *s3);
+
+IntSet *Difference(IntSet *s1, const IntSet *s2, const IntSet *s3);
+
+void PrintLn(const IntSet *s);
+
+void Terminate(IntSet *s);
+#endif
+
+
