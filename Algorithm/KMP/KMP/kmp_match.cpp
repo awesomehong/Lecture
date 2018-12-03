7c5dc0426bd3feb9e21d85b31ab75f3a1dee7c36
awesomehong0611@gmail.com <awesomehong0611@gmail.com>
2018-12-03 14:30:10 +0900

    KMP

---
+++ Algorithm/KMP/KMP/kmp_match.cpp
@@ -0,0 +1,31 @@
+int kmp_match(const char txt[], const char pat[])
+{
+	int pt = 1;
+	int pp = 0;
+	int skip[1024];
+
+	skip[pt] = 0;
+	while (pat[pt] != '\0') {
+		if (pat[pt] == pat[pp])
+			skip[++pt] = ++pp;
+		else if (pp == 0)
+			skip[++pt] = pp;
+		else
+			pp = skip[pp];
+	}
+
+	pt = pp = 0;
+	while (txt[pt] != '\0' && pat[pp] != '\0') {
+		if (txt[pt] == pat[pp]) {
+			pt++; pp++;
+		}
+		else if (pp == 0)
+			pt++;
+		else
+			pp = skip[pp];
+	}
+	if (pat[pp] == '\0')
+		return pt - pp;
+
+	return -1;
+}
