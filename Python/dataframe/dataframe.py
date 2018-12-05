2aeabdb90df1435d4f3824c853b200336c20f670
awesomehong <duldakr0611@cu.ac.kr>
2018-12-05 16:13:28 +0900

    Dataframe

--- Python/dataframe/dataframe.py
+++ Python/dataframe/dataframe.py
@@ -15,16 +15,22 @@ print(alco2009.min(axis=1))
 print(alco2009.sum())
 
 
+alco = pd.read_csv("niaaa-report.csv", index_col=["State", "Year"])
+
+alco["Total"] = alco.Wine + alco.Spirits + alco2009
+print(alco.head())
+
+alco_noidx = alco.reset_index()
+sum_alco = alco_noidx.groupby("Year").count()
+print(sum_alco.tail())
+
+wine_state = alco2009["Wine"] > alco2009["Wine"].mean()
+beer_state = alco2009["Beer"] > alco2009["Beer"].mean()
+pd.crosstab(wine_state, beer_state)
 
 dna = "AGTCCGCGAATACAGGCTCGGT"
 dna1 = dna.replace("C", "")
 dna2 = dna.replace("T", "")
 dna_as_series1 = pd.Series(list(dna1), name="genes")
 dna_as_series2 = pd.Series(list(dna2), name="genes")
-dna_as_series1.value_counts() + dna_as_serires2.value_counts()
-
-dna_as_series.unique()
-dna_as_series.value_counts().sort_index()
-
-valid_nucs = list("ACGT")
-dna_as_series.isin(valid_nucs).all()
+dna_as_series1.value_counts() + dna_as_series2.value_counts()
