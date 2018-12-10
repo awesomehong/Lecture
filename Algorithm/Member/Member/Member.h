440e0561688d8e347f07e70cee05338a55ff108c
awesomehong <duldakr0611@cu.ac.kr>
2018-12-10 13:50:31 +0900

    Member

---
+++ Algorithm/Member/Member/Member.h
@@ -0,0 +1,22 @@
+#ifndef ___Member
+#define ___Member
+
+typedef struct {
+	int no;
+	char name[20];
+}Member;
+
+#define MEMBER_NO 1
+
+#define MEMBER_NAME 2
+
+int MemberNoCmp(const Member *x, const Member *y);
+
+int MemberNameCmp(const Member *x, const Member *y);
+
+void PrintMember(const Member *x);
+
+void PrintLnMember(const Member *x);
+
+Member ScanMemeber(const char *message, int sw);
+#endif
